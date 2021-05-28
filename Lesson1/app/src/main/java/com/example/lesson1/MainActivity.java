cb2c2c8e9d173c306cf28df7c55bb95e3537f7e7
joltyi <m.nosov@hotmail.com>
2021-05-28 13:40:47 -0500

    Lesson1 Task2 Task3

--- Lesson1/app/src/main/java/com/example/lesson1/MainActivity.java
+++ Lesson1/app/src/main/java/com/example/lesson1/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.Switch;
@@ -14,6 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button1);
+        button.setOnClickListener(view -> {
+            setContentView(R.layout.activity_edit_text);
+        });
 
         //Switch
         Switch sw = findViewById(R.id.switch1);
