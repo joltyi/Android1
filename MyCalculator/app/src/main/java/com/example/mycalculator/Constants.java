92bf1e4a29b70b895189dd1a65b39ab7f92b4275
joltyi <m.nosov@hotmail.com>
2021-06-07 07:58:03 +0300

    Lesson2: added save and restore instance state

---
+++ MyCalculator/app/src/main/java/com/example/mycalculator/Constants.java
@@ -0,0 +1,10 @@
+package com.example.mycalculator;
+
+public class Constants {
+    public static final String KEY_OPERAND1 = "Operand1_Text_View";
+    public static final String KEY_OPERAND2 = "Operand2_Text_View";
+    public static final String KEY_OPERATOR = "Operator_Text_View";
+    public static final String KEY_RESULT = "Result_Text_View";
+    public static final String KEY_COMPLETE_OPERATION = "Compete_Operation_Text_View";
+    public static final String KEY_OPERATOR_VALUE = "Operator_Value";
+}
--- MyCalculator/app/src/main/java/com/example/mycalculator/MainActivity.java
+++ MyCalculator/app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mycalculator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -9,6 +10,13 @@ import android.widget.Toast;
 
 import java.util.Arrays;
 import java.util.Locale;
+
+import static com.example.mycalculator.Constants.KEY_COMPLETE_OPERATION;
+import static com.example.mycalculator.Constants.KEY_OPERAND1;
+import static com.example.mycalculator.Constants.KEY_OPERAND2;
+import static com.example.mycalculator.Constants.KEY_OPERATOR;
+import static com.example.mycalculator.Constants.KEY_OPERATOR_VALUE;
+import static com.example.mycalculator.Constants.KEY_RESULT;
 
 public class MainActivity extends AppCompatActivity {
     private final static int MAX_OPERAND_LENGTH = 15;
@@ -32,6 +40,28 @@ public class MainActivity extends AppCompatActivity {
         resultTextView = findViewById(R.id.result);
         operator = Operator.NULL;
         initButtons();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(KEY_OPERAND1, operandOneTextView.getText().toString());
+        outState.putString(KEY_OPERAND2, operandTwoTextView.getText().toString());
+        outState.putString(KEY_OPERATOR, operatorTextView.getText().toString());
+        outState.putString(KEY_RESULT, resultTextView.getText().toString());
+        outState.putString(KEY_COMPLETE_OPERATION, completeOperationTextView.getText().toString());
+        outState.putString(KEY_OPERATOR_VALUE, operator.getOperatorString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        operandOneTextView.setText(savedInstanceState.getString(KEY_OPERAND1, ""));
+        operandTwoTextView.setText(savedInstanceState.getString(KEY_OPERAND2, ""));
+        operatorTextView.setText(savedInstanceState.getString(KEY_OPERATOR, ""));
+        resultTextView.setText(savedInstanceState.getString(KEY_RESULT, ""));
+        completeOperationTextView.setText(savedInstanceState.getString(KEY_COMPLETE_OPERATION, ""));
+        operator = getOperatorFromString(savedInstanceState.getString(KEY_OPERATOR_VALUE, ""));
     }
 
     private void initButtons() {
