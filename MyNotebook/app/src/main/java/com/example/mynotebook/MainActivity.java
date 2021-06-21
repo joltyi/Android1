92a80dcf140bd1ab78486ed3ab1ad859fa008f20
joltyi <m.nosov@hotmail.com>
2021-06-21 20:37:03 +0300

    Lesson7: added main menu

--- MyNotebook/app/src/main/java/com/example/mynotebook/MainActivity.java
+++ MyNotebook/app/src/main/java/com/example/mynotebook/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.mynotebook;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,5 +28,35 @@ public class MainActivity extends AppCompatActivity {
         NotesListFragment listFragment = new NotesListFragment();
         fragmentTransaction.add(R.id.notes_list_layout, listFragment);
         fragmentTransaction.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        MenuItem search = menu.findItem(R.id.menu_search);
+        SearchView searchText = (SearchView) search.getActionView();
+        searchText.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(MainActivity.this, query, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return true;
+            }
+        });
+        MenuItem sort = menu.findItem(R.id.menu_sort);
+        sort.setOnMenuItemClickListener(item -> {
+            Toast.makeText(MainActivity.this, R.string.menu_sort, Toast.LENGTH_SHORT).show();
+            return true;
+        });
+        MenuItem send = menu.findItem(R.id.menu_add);
+        send.setOnMenuItemClickListener(item -> {
+            Toast.makeText(MainActivity.this, R.string.menu_add, Toast.LENGTH_SHORT).show();
+            return true;
+        });
+        return true;
     }
 }
